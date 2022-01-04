fa507ce476adf8af57690b204a48e3d512f0344c
JohnPena0115 <calmwinds06@gmail.com>
2022-01-03 20:41:59 -0500

    "Uploading changes"

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        String firstName = "John";
+
+        String lastName = "Pena";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
     }
 }
