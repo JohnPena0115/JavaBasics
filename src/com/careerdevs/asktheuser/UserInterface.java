be851c7acb5dee905715cb293f43d90c247cb194
JohnPena0115 <calmwinds06@gmail.com>
2022-01-22 23:55:12 -0500

    "Uploaded updated User Interface challenge"

--- src/com/careerdevs/asktheuser/UserInterface.java
+++ src/com/careerdevs/asktheuser/UserInterface.java
@@ -9,7 +9,7 @@ public class UserInterface {
 
         Scanner read = new Scanner(System.in);
 
-        int notValid = 0;
+        int failedAttempts = 0;
 
         System.out.print("Welcome to the TI-84 challenge. Are you ready to proceed? (Y or N): ");
 
@@ -17,52 +17,55 @@ public class UserInterface {
         while (true) {
 
             String isValid = read.nextLine();
-            char lowerCase = Character.toLowerCase(isValid.charAt(0)); 
+
+            if (isValid.length() == 0) {
+                System.out.println("");
+                failedAttempts++;
+                if (failedAttempts != 3){
+                    System.out.println("Please only enter 'Y' or 'N': ");
+                } else {
+                    System.out.println("You have committed a David. Hal-9000 will be speaking with you.");
+                    break;
+                }
+                continue;
+            }
+
+            char lowerCase = Character.toLowerCase(isValid.charAt(0));
 
             if (isValid.length() > 1 ||
                     Character.isDigit(isValid.charAt(0)) ||
-                    lowerCase != 'y' && lowerCase != 'n'){
+                    lowerCase != 'y' && lowerCase != 'n' ) {
 
-               if (notValid != 2) {
-
-                   System.out.println("");
-                   System.out.print("Please only enter 'Y' or 'N': ");
-               }
-
-                ++notValid;
-
-                if (notValid == 3) {
-
+                failedAttempts++;
+                if (failedAttempts != 3) {
+                    System.out.println("");
+                    System.out.print("Please only enter 'Y' or 'N': ");
+                } else {
                     System.out.println("");
                     System.out.println("You have committed a David. Hal-9000 will be speaking with you.");
                     break;
                 }
 
-            } else {
 
-               
+            }else{
 
                 if (Character.toLowerCase(lowerCase) == 'y') {
 
                     System.out.println("");
                     System.out.println("""
-                            Great! I can see you have great enthusiasm and confidence
-                            in the mission. Let's proceed forward.""");
+                        Great! I can see you have great enthusiasm and confidence
+                        in the mission. Let's proceed forward.""");
                     break;
-
                 } else if (Character.toLowerCase(lowerCase) == 'n') {
 
                     System.out.println("");
                     System.out.println("""
-                            This mission is too important for me to allow you to
-                            to jeopardize it. The floor hatch will open soon.
-                            Safe travels.""");
+                        This mission is too important for me to allow you to
+                        to jeopardize it. The floor hatch will open soon.
+                        Safe travels.""");
                     break;
-
                 }
-
             }
-
         }
     }
 }
