454bf3e93d170662c185bfda031705e1e57cb8a2
JohnPena0115 <calmwinds06@gmail.com>
2022-01-25 19:01:39 -0500

    "Uploading Part B of the Create a Car challenge with the corresponding changes to the Car class."

--- src/com/careerdevs/Car.java
+++ src/com/careerdevs/Car.java
@@ -1,5 +1,7 @@
 package com.careerdevs;
 
+import java.text.NumberFormat;
+
 public class Car {
 
     public String make;
@@ -7,11 +9,25 @@ public class Car {
     public int mileage;
     public float gasTankPercent;
 
-    Car (String make, String model, int mileage, float gasTankPercent){
+    public Car(String make, String model, int mileage, float gasTankPercent){
 
         this.make = make;
         this.model = model;
         this.mileage = mileage;
         this.gasTankPercent = gasTankPercent;
     }
+
+    NumberFormat miles = NumberFormat.getInstance();
+    NumberFormat percent = NumberFormat.getPercentInstance();
+
+
+    @Override
+    public String toString(){
+
+        return "The " + make + " " + model + " was returned with an odometer reading of "
+                + miles.format(mileage) + " and " + percent.format(gasTankPercent) +
+                " of fuel left in the tank.";
+    }
+
+
 }
--- src/com/careerdevs/carfactory/Main.java
+++ src/com/careerdevs/carfactory/Main.java
@@ -1,12 +1,16 @@
 package com.careerdevs.carfactory;
 
+import com.careerdevs.Car;
+
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        donationRequest();
+        //donationRequest();
+        createACarObject();
 
     }
 
@@ -17,15 +21,20 @@ public class Main {
 
             Scanner read = new Scanner(System.in);
 
-            System.out.print("Hello. We have e-mailed this form because you have expressed\n" +
-                    "interest in donating your car.\n" +
-                    "Do you still wish to continue? (Y or N): ");
+            System.out.println("Hello. We have e-mailed this form because you have expressed\n" +
+                    "interest in donating your car.");
+
+            System.out.println("");
+
+            System.out.print("Do you still wish to continue? (Y or N): ");
+
 
             char confirmation = read.nextLine().charAt(0);
 
 
             if (Character.toLowerCase(confirmation) == 'n') {
 
+                System.out.println("");
                 System.out.println("We are sorry to hear that.\n" +
                                 "If at any point you change your mind\n"+
                                 "do not hesitate to reach out.");
@@ -33,21 +42,20 @@ public class Main {
 
             }
 
+            System.out.println("");
             System.out.print("Great! What is your legal name?: ");
             String owner = read.nextLine();
 
-
-
+            System.out.println("");
             System.out.print("What is the make and model of the car you wish to donate?: ");
 
 
             String[] car = read.nextLine().split(" ");
-
-
             String make = car[0];
             String model = car[1];
 
-        System.out.println("Could you please confirm the following: " + carDonation(owner, make, model));
+            System.out.println("");
+            System.out.println("Could you please confirm the following: " + carDonation(owner, make, model));
 
     }
 
@@ -56,6 +64,32 @@ public class Main {
     public static String carDonation(String owner, String make, String model){
 
         return "I, " + owner + ", wish to donate my " + make + " " + model + " to the Make-A-Wish foundation.";
+
+    }
+
+    public static void createACarObject(){
+
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("");
+        System.out.print("What is the make and model of the car you are returning?");
+
+        String[] carReturned = read.nextLine().split(" ");
+        String make = carReturned[0];
+        String model = carReturned[1];
+
+        System.out.println("");
+        System.out.print("What is the current mileage on the odometer?");
+        int mileage = read.nextInt();
+
+        System.out.println("");
+        System.out.print("In decimal format, roughly, what percentage of fuel is left in the tank?");
+        float fuelRemaining = read.nextFloat();
+
+        Car returnedCar = new Car(make, model, mileage, fuelRemaining);
+
+        System.out.println("");
+        System.out.println(returnedCar.toString());
 
     }
 
