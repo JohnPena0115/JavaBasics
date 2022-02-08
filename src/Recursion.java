a22c74b9322947f793f03582d163235c3587c8d5
JohnPena0115 <calmwinds06@gmail.com>
2022-02-08 18:11:04 -0500

    "Uploading countdownIterative method."

--- src/Recursion.java
+++ src/Recursion.java
@@ -2,21 +2,41 @@ public class Recursion {
 
     public static void main(String[] args) {
 
-        countDown(15);
+        countdownRecursive(15);
+        countdownIterative(4);
 
     }
 
-    public static void countDown(int number){
+    public static void countdownRecursive(int number){
 
         if (number == 0) {
 
-            System.out.println(number);
+            System.out.println(number + "\n");
             return;
 
         }else {
 
             System.out.println(number);
-            countDown(number-1);
+            countdownRecursive(number-1);
+        }
+    }
+
+    public static void countdownIterative(int number){
+
+        while(number >= 0) {
+
+            if(number == 0) {
+
+                System.out.println(number + "\n");
+                number--;
+
+            } else {
+
+                System.out.println(number);
+                number--;
+
+            }
+
         }
     }
 }
