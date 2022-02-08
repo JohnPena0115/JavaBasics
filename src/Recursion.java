bd2faf97ed675c3a82862bf21b2a97afe5b39d2a
JohnPena0115 <calmwinds06@gmail.com>
2022-02-08 17:58:55 -0500

    "Uploading countDown Method."

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,2 +1,22 @@
 public class Recursion {
+
+    public static void main(String[] args) {
+
+        countDown(15);
+
+    }
+
+    public static void countDown(int number){
+
+        if (number == 0) {
+
+            System.out.println(number);
+            return;
+
+        }else {
+
+            System.out.println(number);
+            countDown(number-1);
+        }
+    }
 }
